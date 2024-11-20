ed80083abece59c565840e0e608815d16b73f1e1
Ashutosh Singh Adhikari <113494449+Ashutosh9110@users.noreply.github.com>
2024-11-20 11:39:19 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,13 +8,7 @@ const PORT=process.env.PORT || 3000
 connectDb()
 
 app.use(express.json())
-app.use(cors(
-    {
-        origin: ["https://deploy-mern-1whq.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+app.use(cors());
 app.use(express.static("public"))
 
 app.use("/", require("./routes/user"))
